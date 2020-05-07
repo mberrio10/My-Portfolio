a91fbc21329b822ebbb42a615ebf40a29831d8a1
Mike Berrio <mikeberrio10@hotmail.com>
2020-05-07 02:42:13 +0000

    banner added

--- src/js/component/Components/Banner.js
+++ src/js/component/Components/Banner.js
@@ -1,13 +1,26 @@
 import React from "react";
+import bgBanner from "../../../img/bg-banner.jpg";
+import myAvatar from "../../../img/myAvatar.png";
 
 export class Banner extends React.Component {
 	render() {
 		return (
-			<div className="jumbotron jumbotron-fluid">
-				<h1 className="display-4">
-					We Make <br />
-					<span>businesses</span> spotless
-				</h1>
+			<div className="jumbotron p-4 p-md-5 text-white rounded bg-dark">
+				<img className="bgBanner" src={bgBanner} />
+				<div className="container-fluid">
+					<div className="row mb-2 justify-content-end">
+						<div className="col-lg-7 px-0">
+							<h1 className="display-4 text-right">
+								Hey! I`m Mike B. <br />
+								<span>Full Stack Web Developer</span>
+							</h1>
+							<p className="lead my-3 text-right font-italic">
+								Here to make your bussines Groovy
+							</p>
+						</div>
+					</div>
+				</div>
+				<img className="myAvatar" src={myAvatar} />
 			</div>
 		);
 	}
